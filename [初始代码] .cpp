78568e35234c756f2d6f1d3bebfbeb21a73ddb90
Durian <GoDongmao@outlook.com>
2026-08-15 12:48:43 +0800

    上传文件🍕
    Initial setup of the C++ program with basic includes and I/O optimizations.

---
+++ [初始代码] .cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    // freopen("test.in", "r", stdin);
+    // freopen("test.out", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    return 0;
+}
